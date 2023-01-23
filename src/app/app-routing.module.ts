1988843eca831f2d804959ef0f08cb7f1c69881f
Clarence Palad Ocampo <clarenceocampo25@gmail.com>
2023-01-23 13:04:02 +0100

    Creacion vistas con navegacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './views/about-us/about-us.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
 import { RegisterComponent } from './views/register/register.component';
 
 const routes: Routes = [
-  {path:"",redirectTo:"/register",pathMatch:'full'},
+  {path:"",redirectTo:"/home",pathMatch:'full'},
   {path:"login",component:LoginComponent},
-  {path:"register",component:RegisterComponent}
+  {path:"register",component:RegisterComponent},
+  {path:"home",component:HomeComponent},
+  {path:"about",component:AboutUsComponent},
+  {path:"contact",component:ContactComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,26 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { ProfileComponent } from './components/profile/profile.component';
 import { LoginComponent } from './views/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RegisterComponent } from './views/register/register.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CarrousselComponent } from './components/carroussel/carroussel.component';
+import { HomeComponent } from './views/home/home.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { AboutUsComponent } from './views/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutUsComponent,
-    ContactComponent,
-    ProfileComponent,
     LoginComponent,
     HeaderComponent,
     RegisterComponent,
     FooterComponent,
-    CarrousselComponent
+    CarrousselComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
