5324750b6953a4adf9ee7a8ff2fdc95d63788b59
Clarence Palad Ocampo <clarenceocampo25@gmail.com>
2023-02-22 17:44:23 +0100

    Obtencion de datos del form

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  constructor() { }
+export class LoginComponent {
 
-  ngOnInit(): void {
+  public user: User = new User();
+
+  constructor(private authService: AuthService, private route: Router) { }
+
+  login() {
+    this.authService.login(this.user).subscribe({
+      next: (res) => { 
+        this.route.navigate(['home']);
+        Swal.fire('Success',res.message,'success');
+      },
+      error: (err) => {
+        Swal.fire(err.error.message, err.error.error, 'error');
+      }
+    })
   }
-
 }
