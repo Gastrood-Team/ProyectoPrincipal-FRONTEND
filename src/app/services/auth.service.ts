05300c453f3d2e569635424fb73c230a10a2338e
Clarence Palad Ocampo <clarenceocampo25@gmail.com>
2023-02-22 17:14:14 +0100

    Servicios

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,27 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { catchError, Observable, throwError } from "rxjs";
+import Swal from "sweetalert2";
+import { User } from "../models/user";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthService {
+@Injectable({providedIn:'root'})
 
-  private urlEndPoint: string = 'http://127.0.0.1:8000/login';
+export class AuthService{
 
-  constructor(private http: HttpClient) { }
+    private urlEndPoint: string = 'http://127.0.0.1:8000/login';
 
-  login(email: string, password: string): Observable<any>{
-    let $data = {"email":email,"password":password};
-    return this.http.post(`${this.urlEndPoint}`,$data)
-  }
+    constructor(private http: HttpClient, private route: Router) { }
+
+    login(user: User): Observable<any>{
+        return this.http.post(`${this.urlEndPoint}`, user).pipe(
+            catchError( e => {
+                if(e.status == 401){
+                    return throwError(() => e);
+                }
+                Swal.fire(e.error.menssage, e.error.error, 'error');
+                return throwError(() => e);
+            })
+        );
+    }
 }
--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Observable, catchError, throwError } from 'rxjs';
+import Swal from 'sweetalert2';
 import { Profile } from '../models/profile';
 
 @Injectable({
@@ -8,12 +10,26 @@ import { Profile } from '../models/profile';
 })
 export class ProfileService {
 
-  private urlEndPoint: string = 'http://127.0.0.1:8000/profile';
+  private urlEndPoint: string = "http://127.0.0.1:8000/profile"
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private route: Router) { }
 
-  getProfileById(id: number): Observable<any>{
-    return this.http.get<any>(`${this.urlEndPoint}/${id}`);
+  getProfilebyId(id: number): Observable<any> {
+    return this.http.get<any>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError(e => {
+        Swal.fire(e.error.message, e.error.error, 'error');
+        this.route.navigate(['/home']);
+        return throwError(() => e);
+      })
+    )
   }
 
+  update(profile: Profile): Observable<any> {
+    return this.http.put<any>(`${this.urlEndPoint}/${profile.id}`, profile).pipe(
+      catchError(e => {
+        Swal.fire(e.error.message, e.error.error, 'error');
+        return throwError(() => e);
+      })
+    )
+  }
 }
