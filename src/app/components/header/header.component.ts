1360b61d1073bab76bf3076e34d4f12a2d65aa3e
JOM3C4 <jordimejia655@gmail.com>
2023-03-29 14:06:38 +0200

    updates

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,27 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { ContentService } from 'src/app/services/content.service';
-import { AuthService }  from 'src/app/services/auth.service';
+import { ContentService } from 'src/app/services/content.service'; // Importamos el servicio ContentService para obtener contenido del servidor
+import { AuthService } from 'src/app/services/auth.service'; // Importamos el servicio AuthService para gestionar la autenticación del usuario
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: 'app-header', // Selector del componente
+  templateUrl: './header.component.html', // Ruta del template HTML
+  styleUrls: ['./header.component.css'] // Array con las rutas de los archivos CSS
 })
 export class HeaderComponent implements OnInit {
 
-  header:any;
-  display:boolean = false;
+  header: any; // Variable para almacenar los datos del header obtenidos del servidor
+  display: boolean = false; // Variable para indicar si se debe mostrar el menú o no  
 
-  constructor(private ContentService: ContentService) { }
+  constructor(private ContentService: ContentService, public authService: AuthService) {
+  } // Constructor que recibe los servicios ContentService y AuthService
 
-  showMenu(){
-    this.display = !this.display;
+  showMenu() { // Método para mostrar/ocultar el menú
+    this.display = !this.display; // Invertimos el valor de la variable display
   }
 
-  ngOnInit(): void {
-    this.ContentService.getContent().subscribe((contents:any)=>{
-        this.header = contents.header;
-      }
-    );
+  ngOnInit(): void { // Método OnInit que se ejecuta al inicializar el componente
+    this.ContentService.getContent().subscribe((contents: any) => { // Obtenemos el contenido del servidor
+      this.header = contents.header; // Almacenamos los datos del header en la variable header
+    } );
+    
+    this.display = !this.authService.isLoggedIn(); // Inicializamos la variable display con el valor opuesto al de authService.isLoggedIn()
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,22 +1,33 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
-import { catchError, Observable, throwError } from "rxjs";
+import { catchError, map, Observable, throwError } from "rxjs";
 import Swal from "sweetalert2";
 import { User } from "../models/user";
 
-@Injectable({providedIn:'root'})
+@Injectable({ providedIn: 'root' })
+export class AuthService {
 
-export class AuthService{
-
+    // URL del endpoint para iniciar sesión
     private urlEndPoint: string = 'http://127.0.0.1:8000/login';
 
-    constructor(private http: HttpClient, private route: Router) { }
+    // Variable que indica si el usuario ha iniciado sesión o no
+    private loggedIn: boolean = false;
 
-    login(user: User): Observable<any>{
+    constructor(private http: HttpClient, private router: Router) { }
+
+    // Función para iniciar sesión
+    login(user: User): Observable<any> {
         return this.http.post(`${this.urlEndPoint}`, user).pipe(
-            catchError( e => {
-                if(e.status == 401){
+            // Si la petición se resuelve correctamente, se guarda el token en el local storage y se indica que el usuario ha iniciado sesión
+            map((response: any) => {
+                localStorage.setItem('token', response.access_token);
+                this.loggedIn = true;
+                return response;
+            }),
+            // Si la petición devuelve un error, se maneja el error mostrando un mensaje de alerta en la pantalla
+            catchError((e) => {
+                if (e.status == 401) {
                     return throwError(() => e);
                 }
                 Swal.fire(e.error.menssage, e.error.error, 'error');
@@ -24,4 +35,33 @@ export class AuthService{
             })
         );
     }
+
+    // Método para hacer logout
+    logout() {
+        localStorage.removeItem('token'); // Borramos el token del local storage
+        this.loggedIn = false; // Indicamos que el usuario no está autenticado
+        this.router.navigate(['/']); // Navegamos a la página principal
+    }
+
+    // Método para obtener el token del local storage
+    getToken() {
+        return localStorage.getItem('token'); // Devolvemos el token
+    }
+
+    // Método para comprobar si el usuario está autenticado o no
+    isAuthenticated() {
+        const token = this.getToken(); // Obtenemos el token del local storage
+        if (token) { // Si hay un token
+            // por ahora solo devolvemos true si hay un token almacenado en el local storage
+            return true; // Devolvemos true
+        } else { // Si no hay un token
+            return false; // Devolvemos false
+        }
+    }
+
+    // Método para comprobar si el usuario está autenticado
+    isLoggedIn() {
+        return this.loggedIn; // Devolvemos el valor de la variable loggedIn que indica si el usuario está autenticado o no.
+    }
+    
 }
